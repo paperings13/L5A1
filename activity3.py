a15ef232e93a6821ea30a79e97be2d71daa67fc8
paperings13 <srkprincess369@gmail.com>
2025-04-25 08:55:00 +0530

    second

---
+++ activity3.py
@@ -0,0 +1,6 @@
+print(" Checking the number greater or smaller ")
+num = int(input("Enter a number"))
+if num > 15 :
+    print("The number is greater than 15")
+else:
+    print("The number is smaller than 15")
